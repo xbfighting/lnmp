bd9ca83f80fc6531300cd439b52b4045030bea36
wangbj <wangbaojian@wangbaojiandeMacBook-Air.local>
2017-03-11 15:31:19 +0800

    modify install.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#===============================================================================#
+#   System Required:  CentOS/RadHat 5+                                          #
+#   Description:  Uninstall LAMP(Linux + Nginx + MySQL + PHP )                  #
+#   Author: GufengWang <cn.wangbj@icloud.com>                                   #
+#   Intro:  https://diycode.me                                                  #
+#===============================================================================#
+cur_dir=`pwd`
+
+#include lamp module
+include(){
+    local include=$1
+    if [[ -s ${cur_dir}/include/${include}.sh ]];then
+        . ${cur_dir}/include/${include}.sh
+    else
+        echo "Error:${cur_dir}/include/${include}.sh not found, shell can not be executed."
+        exit 1
+    fi
+}
+
+#lamp main process
+lamp(){
+    include config
+    include public
+    include nginx
+    include php5
+    include mysql
+    echo "Game is over!";
+    exit;
+    clear
+    echo "#####################################################################"
+    echo "# Auto Install LAMP(Linux + Nginx + MySQL + PHP )                   #"
+    echo "# Intro:  https://diycode.me                                        #"
+    echo "# Author: GufengWang <cn.wangbj@icloud.com>                         #"
+    echo "#####################################################################"
+}
+
+#Run it
+lamp 2>&1 | tee -a
